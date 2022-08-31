6466f3bc37d4a2b6644b88b492cd8ff04d09c1f0
JuliusDanes <julius.danes.nugroho@gmail.com>
2022-08-31 18:53:28 +0700

    add files task2-pre_mcc_metrodata

---
+++ taks2-pre_mcc_metrodata/PlaneFigure.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taks2_pre_mcc_metrodata
+{
+    public abstract class PlaneFigure
+    {
+        public abstract double GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("Proccessing Calculate.....");
+        }
+    }
+
+    public class Square : PlaneFigure
+    {
+        private double Side { get; set; }
+
+        public Square(double _side)
+        {
+            Side = _side;
+        }
+        public Square()
+        {
+            Side = 1;
+        }
+
+        public override double GetArea()
+        {
+            double area = Side * Side;
+            return area;
+        }
+    }
+
+    public class Circle : PlaneFigure
+    {
+        private double Radius { get; set; }
+
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+        public Circle()
+        {
+            Radius = 1;
+        }
+        public override double GetArea()
+        {
+            double area = Radius * Radius * Math.PI;
+            return area;
+        }
+    }
+
+    public class Triangle : PlaneFigure
+    {
+        private double Width { get; set; }
+        private double Height { get; set; }
+
+        public Triangle(double _side1, double _side2)
+        {
+            Width = _side1;
+            Height = _side2;
+        }
+
+        public Triangle(double _side1)
+        {
+            Width = _side1;
+            Height = _side1;
+        }
+
+        public Triangle()
+        {
+            Width = 1;
+            Height = 1;
+        }
+
+        public override double GetArea()
+        {
+            double area = (Width * Height) / 2;
+            return area;
+        }
+    }
+
+    public class Rectangle : PlaneFigure
+    {
+        private double Length { get; set; }
+        private double Width { get; set; }
+
+        public Rectangle(double _side1, double _side2)
+        {
+            Length = _side1;
+            Width = _side2;
+        }
+        public Rectangle(double _side1)
+        {
+            Length = _side1;
+            Width = 1;
+        }
+        public Rectangle(float _side2)
+        {
+            Length = 1;
+            Width = _side2;
+        }
+        public Rectangle()
+        {
+            Length = 1;
+            Width = 1;
+        }
+
+        public override double GetArea()
+        {
+            double area = Length * Width;
+            return area;
+        }
+    }
+}
---
+++ taks2-pre_mcc_metrodata/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace taks2_pre_mcc_metrodata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Menghitung Luas Bangun Datar");
+            Console.WriteLine("------------------------------------\n");
+
+            //  Square
+            Console.Write("Masukkan panjang sisi persegi? ");
+            double side = double.Parse(Console.ReadLine());
+            Square square1 = new Square(side);
+            double squareArea = square1.GetArea();
+            Console.WriteLine("Luas persegi adalah " + squareArea);
+
+            // circle 
+            Console.Write("Masukkan radius lingkaran? ");
+            double radius = double.Parse(Console.ReadLine());
+            Circle circle1 = new Circle(side);
+            double circleArea = circle1.GetArea();
+            Console.WriteLine("Luas lingkaran adalah " + circleArea);
+
+            // Triangle
+            Console.Write("Masukkan alas segitiga? ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan tinggi segitiga? ");
+
+            double height = double.Parse(Console.ReadLine());
+            Triangle triangle1 = new Triangle(width, height);
+            double triangleArea = triangle1.GetArea();
+            Console.WriteLine("Luas segitiga adalah " + triangleArea);
+
+            // Rectangle
+            Console.Write("Masukkan lebar persegi panjang? ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan tinggi persegi panjang? ");
+
+            double width2 = double.Parse(Console.ReadLine());
+            Triangle rectangle1 = new Triangle(length, width2);
+            double rectangleArea = rectangle1.GetArea();
+            Console.WriteLine("Luas persegi panjang adalah " + rectangleArea);
+        }
+    }
+}
